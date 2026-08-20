29b1192b5eefcc72f067ba868d19f94be69c4fe6
Frenkstar24 <fp.theeuwen@gmail.com>
2026-08-20 15:06:39 +0200

    Voeg versieerbare GLI rapportageformats uit bron toe

---
+++ report-templates.js
@@ -0,0 +1,42 @@
+window.GLI_REPORT_TEMPLATES = {
+  version: "2026-08-20",
+  sources: [
+    "2 GLI richtlijn Intake.docx",
+    "1 GLI evaluatie gesprek basis aandachtspunten.docx"
+  ],
+  intake: {
+    label: "GLI intake",
+    sections: [
+      ["Beknopt abstract voor verwijzer", "abstract"],
+      ["Meetwaarden", "measurements"],
+      ["Reden deelname, motivatie en doelen", "motivation"],
+      ["Leefstijlanalyse", "lifestyle"],
+      ["Gezondheid en aandachtspunten", "health"],
+      ["Afstemming en vervolg", "nextSteps"]
+    ]
+  },
+  tussen: {
+    label: "GLI traject evaluatie",
+    sections: [
+      ["Beknopt abstract voor verwijzer", "abstract"],
+      ["Meetwaarden", "measurements"],
+      ["Doelen en voortgang", "progress"],
+      ["Leefstijlgewoonten", "lifestyle"],
+      ["Hindernissen en oplossingen", "barriers"],
+      ["Motivatie en welzijn", "motivation"],
+      ["Toekomst en vervolgstappen", "nextSteps"]
+    ]
+  },
+  eind: {
+    label: "GLI traject eindevaluatie",
+    sections: [
+      ["Beknopt abstract voor verwijzer", "abstract"],
+      ["Meetwaarden en eindresultaat", "measurements"],
+      ["Doelen en behaalde voortgang", "progress"],
+      ["Duurzame leefstijlgewoonten", "lifestyle"],
+      ["Resterende hindernissen en terugvalpreventie", "barriers"],
+      ["Motivatie, welzijn en zelfmanagement", "motivation"],
+      ["Vervolg en nazorg", "nextSteps"]
+    ]
+  }
+};
